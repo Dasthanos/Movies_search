79c035995447d101642f4e71f8c446aed4662603
Dastan <talantbekovdastan0@gmail.com>
2026-01-05 17:45:54 +0600

    тут просто реализовал методы: сортировка по названию сортировка по году выпуска и сортировка по режиссеру

--- src/HomeWork26/Main.java
+++ src/HomeWork26/Main.java
@@ -4,6 +4,7 @@ import HomeWork26.Utils.FileUtil;
 import HomeWork26.Utils.MovieWrapper;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -27,26 +28,65 @@ public class Main {
 //            System.out.println("----------------------");
 //        }
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите название фильма: ");
-        String query = scanner.nextLine();
-        System.out.println("Найдено: ");
-        List<Movie> found = Movie.searchTitile(movies, query);
-        for (Movie movie : found) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Введите название фильма: ");
+//        String query = scanner.nextLine();
+//        System.out.println("Найдено: ");
+//        List<Movie> found = Movie.searchTitile(movies, query);
+//        for (Movie movie : found) {
+//            System.out.println("Название: " + movie.getName());
+//            System.out.println("Год: " + movie.getYear());
+//            System.out.println("Описание: " + movie.getDescription());
+//            System.out.println("Режиссёр: " + movie.getDirector());
+//            System.out.println("Актёры:");
+//
+//            for (Cast c : movie.getCast()) {
+//                System.out.println(" - " + c);
+//            }
+//
+//            System.out.println("----------------------");
+//        }
+//        System.out.println("Сортировка по названию: (от А до Я)");
+//        movies.sort(Comparator.naturalOrder());
+//        for (Movie movie : movies) {
+//            System.out.println("Название: " + movie.getName());
+//            System.out.println("----------------------");
+//        }
+//        System.out.println();
+//        System.out.println("Сортировка по названию: (от Я до А)");
+//        movies.sort(Comparator.reverseOrder());
+//        for (Movie movie : movies) {
+//            System.out.println("Название: " + movie.getName());
+//            System.out.println("----------------------");
+//        }
+//
+//        System.out.println();
+//        System.out.println("Сортировка по году выпуска фильма: ");
+//        Comparator cmp = Comparator.comparingInt(Movie::getYear);
+//        movies.sort(cmp.reversed());
+//        for (Movie movie : movies) {
+//            System.out.println("Название: " + movie.getName());
+//            System.out.println("Год выпуска: " + movie.getYear());
+//            System.out.println("----------------------");
+//        }
+//
+//        System.out.println();
+//        System.out.println("Сортировка по году выпуска фильма (по возрастанию):");
+//        Comparator cmpp = Comparator.comparingInt(Movie::getYear);
+//        movies.sort(cmpp);
+//        for (Movie movie : movies) {
+//            System.out.println("Название: " + movie.getName());
+//            System.out.println("Год выпуска: " + movie.getYear());
+//            System.out.println("----------------------");
+//        }
+
+        System.out.println("Сортировка по режиссеру: (от А до Я)");
+        movies.sort(new Director.MovieDirectorComparator());
+        for (Movie movie : movies) {
             System.out.println("Название: " + movie.getName());
-            System.out.println("Год: " + movie.getYear());
-            System.out.println("Описание: " + movie.getDescription());
-            System.out.println("Режиссёр: " + movie.getDirector());
-            System.out.println("Актёры:");
-
-            for (Cast c : movie.getCast()) {
-                System.out.println(" - " + c);
-            }
-
+            System.out.println("Режиссер " + movie.getDirector().getFullName());
             System.out.println("----------------------");
         }
-
-
 
     }
 }
