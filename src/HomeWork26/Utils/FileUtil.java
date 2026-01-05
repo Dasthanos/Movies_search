5b59746814a4ef0d79ebdba979702fae1d1b707e
Dastan <talantbekovdastan0@gmail.com>
2026-01-05 12:55:51 +0600

    чтобы читать жсонфайл сделал утилит который парсит данные с файла и нужен был враппер чтобы сохранить данные и также сделал метод который записывает обратно в файл лист с объектами

--- src/HomeWork26/Utils/FileUtil.java
+++ src/HomeWork26/Utils/FileUtil.java
@@ -1,4 +1,40 @@
 package HomeWork26.Utils;
 
+import HomeWork26.Movie;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class FileUtil {
+
+    public static List<Movie> readFile(String path) {
+        Gson gson = new Gson();
+        try {
+            String json = Files.readString(Paths.get(path));
+            MovieWrapper movieWrapper = gson.fromJson(json, MovieWrapper.class);
+            return movieWrapper.getMovies();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public static void writeFile(List<Movie> movies){
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        Path path = Paths.get("src/HomeWork23/data/trucks.json");
+        String newJson = gson.toJson(movies);
+        byte[] strToByte = newJson.getBytes();
+        try {
+            Files.write(path, strToByte);
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
---
+++ src/HomeWork26/Utils/MovieWrapper.java
@@ -0,0 +1,13 @@
+package HomeWork26.Utils;
+
+import HomeWork26.Movie;
+
+import java.util.List;
+
+public class MovieWrapper {
+    private List<Movie> movies;
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+}
