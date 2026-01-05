5bc7bc13fc158c9f1be61a0785358990b3b61f60
Dastan <talantbekovdastan0@gmail.com>
2026-01-05 12:56:36 +0600

    В мейне сделал вывод всех фильмов

--- src/HomeWork26/Main.java
+++ src/HomeWork26/Main.java
@@ -1,4 +1,31 @@
 package HomeWork26;
 
+import HomeWork26.Utils.FileUtil;
+import HomeWork26.Utils.MovieWrapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+        List<Movie> movies = FileUtil.readFile("src/HomeWork26/data/Movies.json");
+        System.out.println();
+
+        for (Movie movie : movies) {
+            System.out.println("Название: " + movie.getName());
+            System.out.println("Год: " + movie.getYear());
+            System.out.println("Описание: " + movie.getDescription());
+            System.out.println("Режиссёр: " + movie.getDirector());
+            System.out.println("Актёры:");
+
+            for (Cast c : movie.getCast()) {
+                System.out.println(" - " + c);
+            }
+
+            System.out.println("----------------------");
+        }
+
+
+    }
 }
